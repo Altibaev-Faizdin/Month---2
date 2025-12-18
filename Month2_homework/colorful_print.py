473f10e7fd102a83f1bb8fe22c775a340301c0ca
Алтыбаев Алинур <faizaltibaev@gmail.com>
2025-12-19 02:02:19 +0600

    blessed, import

--- Month2_homework/colorful_print.py
+++ Month2_homework/colorful_print.py
@@ -1,38 +1,23 @@
-# import os
-# from blessed import Terminal
-# from homework_1 import Person
-#
-# os.environ['TERM'] = 'xterm-256color'
-# terminal = Terminal(force_styling=True)
-#
-#
-# fruits_colors = {
-#     'apple🍎': terminal.red,
-#     'banana🍌': terminal.yellow,
-#     'cherry🍒': terminal.crimson,
-#     'grape🍇': terminal.webpurple,
-#     'mango🥭': terminal.orange2,
-#     'orange🍊': terminal.orange,
-#     'peach🍑': terminal.rosybrown2,
-# }
-#
-# print(terminal.bold_underline('Fruit Color Display'))
-# for fruit, color in fruits_colors.items():
-#     print(color(fruit))
-
 from blessed import Terminal
 from homework_1 import Person
 
-# Фрукты с эмоджи
-fruits = ["🍎 Apple", "🍌 Banana", "🍒 Cherry", "🍇 Grape", "🍊 Orange", "🥭 Mango", "🍍 Pineapple"]
-colors = ["red", "yellow", "magenta", "blue", "darkorange", "green", "cyan"]
-
 term = Terminal()
 
-for fruit, color in zip(fruits, colors):
+fruit_colors = {
+    "🍎 Apple": "red",
+    "🍌 Banana": "yellow",
+    "🍒 Cherry": "crimson",
+    "🍇 Grape": "webpurple",
+    "🍊 Orange": "orange2",
+    "🥭 Mango": "orange",
+    "🍍 Pineapple": "rosybrown2"
+}
+
+for fruit, color in fruit_colors.items():
     print(getattr(term, color) + fruit + term.normal)
 
-# Примеры Person
+
+
 person1 = Person("Алинур", '12.05.2000', occupation="Медик", higher_education=True)
 person2 = Person("Акылай", '03.11.1998', occupation="Программист", higher_education=False)
 person3 = Person("Артур", '25.08.2002', occupation="Инжинер", higher_education=True)
@@ -41,5 +26,10 @@ print("\nПредставление объектов:")
 person1.introduce()
 person2.introduce()
 person3.introduce()
+
+
+#ЧТОБЫ ПОЛУЧИТ ОТВЕТ НАДО ПОСТАВИТ ВОТ ЭТОТ КОММАНДУ НА ТЕРМИНАЛ ИЛИ GIT BUSH!!!
+#cd C:\Users\admin\PycharmProjects\Month2
+#python Month2_homework/colorful_print.py
 
 
