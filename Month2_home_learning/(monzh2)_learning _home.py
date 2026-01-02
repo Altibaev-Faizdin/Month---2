b1afe5a3116cb87644233f9451dc76793a3d4a85
Altibaev-Faizdin <faizaltibaev@gmail.com>
2026-01-02 15:19:25 +0600

    ffff

--- Month2_home_learning/(monzh2)_learning _home.py
+++ Month2_home_learning/(monzh2)_learning _home.py
@@ -3720,18 +3720,18 @@ from Month2_homework.homework_1 import person1
 
 
                                         #Homework A-27
-class Book:
-    def __init__(self, title, author):
-        self.title = title
-        self.author = author
+# class Book:
+#     def __init__(self, title, author):
+#         self.title = title
+#         self.author = author
 
-    def __str__(self):
-        return f"Name: {self.title} author: {self.author}"
+#     def __str__(self):
+#         return f"Name: {self.title} author: {self.author}"
 
-book1 = Book("1984", "George Orwell")
-book2 = Book("Dune", "Frank Herbert")
-print(book1)
-print(book2)
+# book1 = Book("1984", "George Orwell")
+# book2 = Book("Dune", "Frank Herbert")
+# print(book1)
+# print(book2)
 
 
 
